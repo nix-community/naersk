6b41d979039e06eef6fbe940d439c9f09427f11a
Nicolas Mattia <nicolas@nmattia.com>
2019-07-02 13:06:30 +0200

    Comment out buildPackageIncremental

--- default.nix
+++ default.nix
@@ -38,27 +38,28 @@ with rec
           };
         import ./build.nix src (defaultAttrs // attrs);
 
-      buildPackageIncremental = cargolock: name: version: src: attrs:
-        with rec
-          { buildDependency = depName: depVersion:
-              # Really this should be 'buildPackageIncremental' but that makes
-              # Nix segfault
-              buildPackage (libb.dummySrc depName depVersion)
-                { cargoBuild = "cargo build --release -p ${depName}:${depVersion} -j $NIX_BUILD_CORES";
-                  inherit (attrs) cargo;
-                  cargotomlPath = libb.writeTOML (libb.cargotomlFor depName depVersion);
-                  cargolockPath = libb.writeTOML (
-                    libb.cargolockFor cargolock depName depVersion
-                    );
-                  doCheck = false;
-                };
-          };
-        buildPackage src (attrs //
-          {
-            builtDependencies = map (x: buildDependency x.name x.version)
-              (libb.directDependencies cargolock name version) ;
-          }
-          );
+# XXX: not quite working yet
+#      buildPackageIncremental = cargolock: name: version: src: attrs:
+#        with rec
+#          { buildDependency = depName: depVersion:
+#              # Really this should be 'buildPackageIncremental' but that makes
+#              # Nix segfault
+#              buildPackage (libb.dummySrc depName depVersion)
+#                { cargoBuild = "cargo build --release -p ${depName}:${depVersion} -j $NIX_BUILD_CORES";
+#                  inherit (attrs) cargo;
+#                  cargotomlPath = libb.writeTOML (libb.cargotomlFor depName depVersion);
+#                  cargolockPath = libb.writeTOML (
+#                    libb.cargolockFor cargolock depName depVersion
+#                    );
+#                  doCheck = false;
+#                };
+#          };
+#        buildPackage src (attrs //
+#          {
+#            builtDependencies = map (x: buildDependency x.name x.version)
+#              (libb.directDependencies cargolock name version) ;
+#          }
+#          );
 
   };
 
