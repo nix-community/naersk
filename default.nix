115b63b5542db25afee4bdadedf9fa17161e093e
Nicolas Mattia <nicolas@nmattia.com>
2019-11-18 14:59:14 +0100

    Re indent

--- default.nix
+++ default.nix
@@ -39,49 +39,47 @@ let
         { inherit lib writeText remarshal runCommand ; }; in
 
 # Crate building
-with rec
-  {
-      mkConfig = src: attrs:
-        import ./config.nix { inherit lib src attrs libb builtinz; };
-      buildPackage = src: attrs:
-        let config = (mkConfig src attrs); in
-        import ./build.nix src
-          (defaultBuildAttrs //
-            { pname = config.packageName;
-              version = config.packageVersion;
-              inherit (config) cratePaths crateDependencies preBuild cargoBuild cargoTestCommands compressTarget;
-            } //
-            (removeAttrs attrs [ "usePureFromTOML" "cargotomls" "singleStep" ]) //
-            { builtDependencies = lib.optional (! config.isSingleStep)
-                (
-                  import ./build.nix
-                  (libb.dummySrc
-                    { cargoconfig =
-                        if builtinz.pathExists (toString src + "/.cargo/config")
-                        then builtins.readFile (src + "/.cargo/config")
-                        else null;
-                      cargolock = config.cargolock;
-                      cargotomls = config.cargotomls;
-                      inherit (config) patchedSources;
-                    }
-                  )
-                  (defaultBuildAttrs //
-                    { pname = "${config.packageName}-deps";
-                      version = config.packageVersion;
-                      inherit (config) cratePaths crateDependencies cargoBuild compressTarget;
-                    } //
-                  (removeAttrs attrs [ "usePureFromTOML" "cargotomls"  "singleStep"]) //
-                  { preBuild = "";
-                    cargoTestCommands = map (cmd: "${cmd} || true") config.cargoTestCommands;
-                    copyTarget = true;
-                    copyBins = false;
-                    copyDocsToSeparateOutput = false;
-                  }
-                  )
-                );
-            });
-  };
-
+let
+  mkConfig = src: attrs:
+    import ./config.nix { inherit lib src attrs libb builtinz; };
+  buildPackage = src: attrs:
+    let config = (mkConfig src attrs); in
+    import ./build.nix src
+      (defaultBuildAttrs //
+        { pname = config.packageName;
+          version = config.packageVersion;
+          inherit (config) cratePaths crateDependencies preBuild cargoBuild cargoTestCommands compressTarget;
+        } //
+        (removeAttrs attrs [ "usePureFromTOML" "cargotomls" "singleStep" ]) //
+        { builtDependencies = lib.optional (! config.isSingleStep)
+            (
+              import ./build.nix
+              (libb.dummySrc
+                { cargoconfig =
+                    if builtinz.pathExists (toString src + "/.cargo/config")
+                    then builtins.readFile (src + "/.cargo/config")
+                    else null;
+                  cargolock = config.cargolock;
+                  cargotomls = config.cargotomls;
+                  inherit (config) patchedSources;
+                }
+              )
+              (defaultBuildAttrs //
+                { pname = "${config.packageName}-deps";
+                  version = config.packageVersion;
+                  inherit (config) cratePaths crateDependencies cargoBuild compressTarget;
+                } //
+              (removeAttrs attrs [ "usePureFromTOML" "cargotomls"  "singleStep"]) //
+              { preBuild = "";
+                cargoTestCommands = map (cmd: "${cmd} || true") config.cargoTestCommands;
+                copyTarget = true;
+                copyBins = false;
+                copyDocsToSeparateOutput = false;
+              }
+              )
+            );
+        });
+in
 {
   inherit buildPackage;
 }
