65e8bba46a0b07c39517bffe01300c53e174f29e
Nicolas Mattia <nicolas@nmattia.com>
2020-02-11 20:28:56 +0100

    Add reproduction for workspace build.rs failure

---
+++ test/workspace-build-rs/crate-a/build.rs
@@ -0,0 +1,16 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("hello.rs");
+    fs::write(
+        &dest_path,
+        "pub fn message() -> &'static str {
+            \"Hello, World!\"
+        }
+        "
+    ).unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+}
