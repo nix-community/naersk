nix-community/naersk

commit 2b41e666c168d430e1408023a3646ec729baa19a

Improve Cargo.toml discovery algorithm
Our current Cargo.toml discovery algorithm is based on reading the main
Cargo.toml and looking at its `workspace.members` - unfortunately, as it
turns out there's actually no requirement for all workspace crates to be
listed there, and some applications do fancy things like:

```toml
[workspace]
members = [ "crates/foo" ]

[dependencies]
foo = { path = "crates/foo" }
bar = { path = "crates/bar" } # whoopsie
```

... which doesn't play nice with Naersk's incremental builds (that is,
the compilation fails unless `singleStep = true;` is turned on).

This commit changes the logic to use a different approach: now we simply
recursively scan the root directory, noting down all the Cargo.tomls we
find; Crane seems to use the same algorithm.

I think the only case where this approach falls short are spurious
Cargo.tomls that are present somewhere in the source directory but are
actually unused - for instance, imagine I'm writing a Cargo clone where
I've got files like `src/tests/broken-manifest/Cargo.toml` - in cases
like these, the current approach will cause the build to fail, because
we will try to fixup a broken Cargo.toml, thinking it's used somewhere.

We could try to handle cases like these by using an even different
approach: instead of traversing the source tree, we could load the main
Cargo.toml and traverse its `workspace.members` + all path-based
dependencies recursively, noting down all the *explicitly present*
Cargo.tomls.

That is, given a top-level manifest of:

```
[workspace]
members = [ "crates/foo" ]

[dependencies]
foo = { path = "crates/foo" }
bar = { path = "crates/bar" }
```

... we'd note down and load `crates/foo/Cargo.toml` +
`crates/bar/Cargo.toml`, and then load _their_ path-based dependencies,
to find all transitive Cargo.tomls that belong to this workspace.

(that is, we have to scan the crates recursively, because `bar` might
depend on `crates/zar` - and that's not an imaginary edge case, that's

config.nix
@@ -116,9 +116,6 @@ let
     # `singleStep` greatly reduces the incrementality of the builds.
     singleStep = attrs0.singleStep or false;
 
-    # The targets to build if the `Cargo.toml` is a virtual manifest.
-    targets = attrs0.targets or null;
-
     # When true, the resulting binaries are copied to `$out/bin`. <br/>
     # Note: this relies on cargo's `--message-format` argument, set in the
     # default `cargoBuildOptions`.
@@ -277,77 +274,48 @@ let
   buildPlanConfig = rec {
     inherit userAttrs;
     inherit (sr) src root;
-    # Whether we skip pre-building the deps
-    isSingleStep = attrs.singleStep;
-
     inherit (attrs) overrideMain gitAllRefs gitSubmodules;
 
-    # The members we want to build
-    # (list of directory names)
-    wantedMembers =
-      lib.mapAttrsToList (member: _cargotoml: member) wantedMemberCargotomls;
+    isSingleStep = attrs.singleStep;
 
-    # Member path to cargotoml
-    # (attrset from directory name to Nix object)
-    wantedMemberCargotomls =
-      let
-        pred =
-          if ! isWorkspace
-          then (_member: _cargotoml: true)
-          else
-            if ! isNull attrs.targets
-            then (_member: cargotoml: lib.elem cargotoml.package.name attrs.targets)
-            else (member: _cargotoml: member != ".");
-      in
-        lib.filterAttrs pred cargotomls;
-
-    # All cargotomls, from path to nix object
-    # (attrset from directory name to Nix object)
+    # List of all the Cargo.tomls in the workspace.
+    #
+    # Note that the simplest thing here would be to read `workspace.members`,
+    # but somewhat unfortunately there's no requirement that all workspace
+    # crates should be listed there - for instance, some projects¹ do:
+    #
+    # ```
+    # [workspace]
+    # members = [ "crates/foo", "crates/bar" ]
+    #
+    # [dependencies]
+    # foo = { path = "crates/foo" }
+    # bar = { path = "crates/bar" }
+    # zar = { path = "crates/zar" }
+    # ```
+    #
+    # ... which Cargo allows and so should we.
+    #
+    # ¹ such as Nushell
     cargotomls =
       let
-        readTOML = builtinz.readTOML usePureFromTOML;
-      in
-        { "." = toplevelCargotoml; } // lib.optionalAttrs isWorkspace
-          (
-            lib.listToAttrs
-              (
-                map
-                  (
-                    member:
-                      {
-                        name = member;
-                        value = readTOML (root + "/${member}/Cargo.toml");
-                      }
-                  )
-                  members
-              )
-          );
+        findCargoTomls = dir:
+          lib.mapAttrsToList
+            (name: type:
+              let
+                path = "${root}/${dir}/${name}";
 
-    # The cargo members
-    members =
-      let
-        # the members, as listed in the virtual manifest
-        listedMembers = toplevelCargotoml.workspace.members or [];
-
-        # this turns members like "foo/*" into [ "foo/bar" "foo/baz" ]
-        # as in https://github.com/rust-analyzer/rust-analyzer/blob/b2ed130ffd9c79de26249a1dfb2a8312d6af12b3/Cargo.toml#L2
-        expandMember = member:
-          if (lib.hasSuffix "/*" member) || (lib.hasSuffix "/*/" member)
-          then
-            let
-              rootDir = lib.replaceStrings [ "/*/" "/*" ] [ "" "" ] member;
-              subdirs = (
-                builtins.attrNames (
-                  lib.filterAttrs
-                    (name: type: type == "directory")
-                    (builtins.readDir (root + "/${rootDir}"))
-                )
-              );
-            in map (subdir: "${rootDir}/${subdir}") subdirs
-          else [ member ];
+              in
+              if type == "regular" && name == "Cargo.toml" then
+                [{ name = dir; toml = readTOML path; }]
+              else if type == "directory" then
+                findCargoTomls "${dir}/${name}"
+              else
+                [])
+            (builtins.readDir "${root}/${dir}");
 
       in
-        lib.unique (lib.concatMap expandMember listedMembers);
+        lib.flatten (findCargoTomls ".");
 
     # If `copySourcesFrom` is set, then it looks like the benefits brought by
     # two-step caching break, for unclear reasons as of now. As such, do not set
@@ -379,13 +347,13 @@ let
     toplevelCargotoml = readTOML (root + "/Cargo.toml");
 
     # The cargo lock
-    cargolock = 
+    cargolock =
       let
         cargolock-file = root + "/Cargo.lock";
       in
       if builtins.pathExists cargolock-file then
         readTOML (cargolock-file)
-      else 
+      else
         throw "Naersk requires Cargo.lock to be available in root. Check that it is not in .gitignore and stage it when using git to filter sources (which flakes does)";
 
     packageName =

test/slow/default.nix
@@ -1,6 +1,7 @@
 args: {
   agent-rs = import ./agent-rs args;
   lorri = import ./lorri args;
+  nushell = import ./nushell args;
   ripgrep-all = import ./ripgrep-all args;
   rustlings = import ./rustlings args;
   talent-plan = import ./talent-plan args;

test/slow/nushell/default.nix
@@ -1,0 +1,29 @@
+{ sources, ... }:
+let
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (import sources.nixpkgs-mozilla)
+    ];
+  };
+
+  toolchain = (pkgs.rustChannelOf {
+    rustToolchain = "${sources.nushell}/rust-toolchain.toml";
+    sha256 = "sha256-Zk2rxv6vwKFkTTidgjPm6gDsseVmmljVt201H7zuDkk=";
+  }).rust;
+
+  naersk = pkgs.callPackage ../../../default.nix {
+    cargo = toolchain;
+    rustc = toolchain;
+  };
+
+  app = naersk.buildPackage {
+    src = sources.nushell;
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+    buildInputs = with pkgs; [ openssl ];
+  };
+
+in
+pkgs.runCommand "nushell-test"
+{
+  buildInputs = [ app ];
+} "nu -c 'echo yes!' && touch $out"

lib.nix
@@ -121,15 +121,16 @@ rec
 
   # A very minimal 'src' which makes cargo happy nonetheless
   dummySrc =
-    { cargoconfig   # string
-    , cargotomls   # attrset
-    , cargolock   # attrset
+    { cargoconfig # string
+    , cargotomls # list
+    , cargolock # attrset
     , copySources # list of paths that should be copied to the output
     , copySourcesFrom # path from which to copy ${copySources}
     }:
       let
         config = writeText "config" cargoconfig;
         cargolock' = builtinz.writeTOML "Cargo.lock" cargolock;
+
         fixupCargoToml = cargotoml:
           let
             attrs =
@@ -143,10 +144,9 @@ rec
               package = removeAttrs attrs.package [ "build" ];
             };
 
-        # a list of tuples from member to cargo toml:
-        #   "foo-member:/path/to/toml bar:/path/to/other-toml"
-        cargotomlss = lib.mapAttrsToList
-          (k: v: "${k}:${builtinz.writeTOML "Cargo.toml" (fixupCargoToml v)}")
+        cargotomlss = map
+          ({ name, toml }:
+            "${name}:${builtinz.writeTOML "Cargo.toml" (fixupCargoToml toml)}")
           cargotomls;
 
       in
@@ -166,11 +166,12 @@ rec
                 final_path="$final_dir/Cargo.toml"
                 cp $cargotoml "$final_path"
 
-                # make sure cargo is happy
                 pushd $out/$member > /dev/null
                 mkdir -p src
+
                 # Avoid accidentally pulling `std` for no-std crates.
                 echo '#![no_std]' >src/lib.rs
+
                 # pretend there's a `build.rs`, otherwise cargo doesn't build
                 # the `[build-dependencies]`. Custom locations of build scripts
                 # aren't an issue because we strip the `build` field in

commit 56dd2df9a61a912ebf9d3d79559aad70d51990de

Fix detection of symlinked Cargo.tomls
This commit fixes a bug introduced in #276 where symlinked Cargo.tomls
wouldn't get detected due to readDir() reporting them as - well -
_symlinks_ instead of regular files.

Note that we continue to keep the `if type == "directory"` condition for
recursion which means that we'll fail to detect "nested" Cargo.tomls if
someone decides to go wild and create workspace with symlinked crates.

That I'm not 100% sure on how to approach, but fortunately cases like
those seem to be practically non-existent (and Crane apparently not
supporting them builds a bit of confidence here as well).

Closes #280.

config.nix
@@ -306,7 +306,7 @@ let
                 path = "${root}/${dir}/${name}";
 
               in
-              if type == "regular" && name == "Cargo.toml" then
+              if name == "Cargo.toml" then
                 [{ name = dir; toml = readTOML path; }]
               else if type == "directory" then
                 findCargoTomls "${dir}/${name}"

test/fast/default.nix
@@ -13,6 +13,7 @@ args: {
   readme = import ./readme args;
   simple-dep = import ./simple-dep args;
   simple-dep-patched = import ./simple-dep-patched args;
+  symlinks = import ./symlinks args;
   workspace = import ./workspace args;
   workspace-build-rs = import ./workspace-build-rs args;
   workspace-patched = import ./workspace-patched args;

test/fast/symlinks/default.nix
@@ -1,0 +1,8 @@
+{ naersk, pkgs, ... }: {
+  default = naersk.buildPackage {
+    src = pkgs.symlinkJoin {
+      name = "src";
+      paths = [ ./fixtures ];
+    };
+  };
+}

test/fast/symlinks/fixtures/Cargo.toml
@@ -1,0 +1,4 @@
+[package]
+name = "app"
+version = "0.1.0"
+edition = "2018"

test/fast/symlinks/fixtures/src/main.rs
@@ -1,0 +1,3 @@
+fn main() {
+    println!("Hello, world!");
+}