78789c30d64dea2396c9da516bbcc8db3a475207
Patryk Wychowaniec <pwychowaniec@pm.me>
2023-08-17 12:39:58 +0200

    Fix support for `.cargo/config`
    Before https://github.com/nix-community/naersk/pull/300, our
    `builtinz.toTOML` used to return a string representing the serialized
    document which was later redirected into a file:
    
    https://github.com/nix-community/naersk/pull/300/files#diff-b6b537316f2d29c8faf178a110366796811d1bc72f694262c7d2efad79aa984bL238
    
    Over that merge request, this was changed to return a path, to make it
    consistent with how nixpkgs' formatters work - but I haven't noticed one
    place that still _read_ the file instead of returning a path (which was
    fine before, since the code did `echo ... > ...`, but is not a correct
    thing to do for `cp`).
    
    Closes https://github.com/nix-community/naersk/issues/305.

---
+++ test/fast/cargo-config/default.nix
@@ -0,0 +1,20 @@
+{ sources, pkgs, ... }:
+
+let
+  fenix = import sources.fenix { };
+
+  # Support for custom environmental variables was introduced in Cargo 1.56 and
+  # our tests use nixpkgs-21.05 which contains an older version of Cargo, making
+  # this test fail otherwise.
+  toolchain = fenix.latest;
+
+  naersk = pkgs.callPackage ../../../default.nix {
+    cargo = toolchain.cargo;
+    rustc = toolchain.rustc;
+  };
+
+in
+naersk.buildPackage {
+  src = ./fixtures;
+  doCheck = true;
+}
