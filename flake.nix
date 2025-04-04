aac678b2288a85f99363a7a3946e7dcad2dab982
Jörg Thalheim <joerg@thalheim.io>
2025-04-04 17:25:51 +0200

    don't use indirect nixpkgs references
    Those are deprecated in Nix:
    
    https://github.com/NixOS/nix/pull/12068

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "Build Rust projects with ease. No configuration, no code generation; IFD and sandbox friendly.";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+
   outputs = { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin" ];
