c4591a90541baf18cd9d29502743cd1219fd14f1
Nicolas Mattia <nicolas@nmattia.com>
2025-07-14 11:17:34 +0200

    Flakify readme

--- test/fast/readme/default.nix
+++ test/fast/readme/default.nix
@@ -1,2 +1,8 @@
-{ ... }:
-(import ../../../readme.nix).test
+{ pkgs, ... }:
+let
+  readme = pkgs.callPackage ../../../readme.nix { };
+in
+pkgs.runCommand "readme-test" { } ''
+  diff ${../../../README.md} ${readme}
+  touch $out
+''
