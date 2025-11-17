65a23867f3063597698eb1461c0697cf83686b5b
link2xt <link2xt@testrun.org>
2025-11-17 11:59:01 +0100

    Use only one version of nixpkgs in cross-windows example

--- examples/cross-windows/flake.nix
+++ examples/cross-windows/flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
     fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
