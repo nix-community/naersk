2aaa18f21bc8bbddf1255f672926812566ca43c1
Nicolas Mattia <nicolas@nmattia.com>
2026-06-11 16:06:08 +0200

    Clarify metabuild workaround

--- test/fast/metabuild/default.nix
+++ test/fast/metabuild/default.nix
@@ -1,23 +1,8 @@
 {
   naersk,
-  fenix,
-  system,
   ...
 }:
-let
-  toolchain =
-    with fenix.packages.${system};
-    combine [
-      latest.rustc
-      latest.cargo
-    ];
 
-  naersk' = naersk.override {
-    cargo = toolchain;
-    rustc = toolchain;
-  };
-in
-
-naersk'.buildPackage {
+naersk.buildPackage {
   src = ./fixtures;
 }
