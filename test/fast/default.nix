cf2acb137174632d532deeb3ad53bda3c64f712a
Nicolas Mattia <nicolas@nmattia.com>
2026-06-23 15:04:17 +0200

    Fix reading workspace version in single Cargo.toml

--- test/fast/default.nix
+++ test/fast/default.nix
@@ -25,5 +25,6 @@
   workspace-build-rs = pkgs.callPackage ./workspace-build-rs { inherit naersk; };
   workspace-deps = pkgs.callPackage ./workspace-deps { inherit naersk; };
   workspace-meta = pkgs.callPackage ./workspace-meta { inherit naersk; };
+  workspace-meta-single = pkgs.callPackage ./workspace-meta-single { inherit naersk; };
   workspace-patched = pkgs.callPackage ./workspace-patched { inherit naersk; };
 }
